abed1ee68467318b6f364825c1304cbbf0f46777
Rodrigo Morais <morais.rm@gmail.com>
2016-01-20 17:54:06 -0200

    First solution to function which get the occurrences of character in an expression.

--- huffman.js
+++ huffman.js
@@ -1,3 +1,23 @@
 function getOccurrences(expression) {
-	return [['A', 6]];
+	var occurrences = [];
+
+	expression.split('').forEach(function(word) {
+		var position = 0;
+
+		if(occurrences.length > 0) {
+			while(position < occurrences.length && occurrences[position][0] != word) {
+				position = position + 1; 
+			}
+		}
+		
+		if(occurrences.length <= position) {
+			occurrences.push([word, 1]);
+		}
+		else {
+			occurrences[position][1] = occurrences[position][1] + 1;
+		}
+
+	});
+
+	return occurrences;
 }
