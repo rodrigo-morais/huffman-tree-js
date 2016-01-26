bf0947ee61d6fa2475835c7a08cf926ba0bd3afc
Rodrigo Morais <morais.rm@gmail.com>
2016-01-26 13:47:32 -0200

    Modifing the function getOccurrences to be more functional.

--- huffman.js
+++ huffman.js
@@ -1,25 +1,14 @@
 function getOccurrences(expression) {
-	var occurrences = [];
-
-	expression.split('').forEach(function(word) {
-		var position = 0;
-
-		if(occurrences.length > 0) {
-			while(position < occurrences.length && occurrences[position][0] != word) {
-				position = position + 1; 
-			}
+	return expression.split('').map(function(letter, position, word) {
+		var occurrences = expression.split('').filter(function(exp) {
+			return exp === letter;
+		}).length;
+		if(word.indexOf(letter) === position){
+			return [letter, occurrences];
 		}
-		
-		if(occurrences.length <= position) {
-			occurrences.push([word, 1]);
-		}
-		else {
-			occurrences[position][1] = occurrences[position][1] + 1;
-		}
-
+	}).filter(function(occurrence){
+		return occurrence;
 	});
-
-	return occurrences;
 }
 
 function orderOccurrences(occurrences) {
